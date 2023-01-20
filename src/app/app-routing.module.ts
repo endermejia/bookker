836acca5aa66bbd9d2144388451bf54740f5531b
Gabriel Mejia <endermejia032@gmail.com>
2023-01-20 18:35:29 +0100

    First commit
    - Angular App
    - Routing
    - HomeComponent
    - FavoritesComponent

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FavoritesComponent} from "./favorites/favorites.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
